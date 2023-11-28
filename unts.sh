a3dea730e958a85decc1ea10a717d2a4775a250b
Hugo Amaro <hugodbamaro@gmail.com>
2023-11-28 10:19:02 +0000

    latest load_midi_bert.py

---
+++ unts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+py_version=$(python3 --version)
+version_number=$(echo $py_version | cut -d' ' -f2)
+
+pyenv virtualenv version_number music_proj_env
+pyenv local music_proj_env
+pip install --upgrade pip
+
+pip install -r requirements.txt
+
+pyenv activate music_proj_env
