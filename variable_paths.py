cde4007f682a4a0d383e34090de45370dbf35949
Deborah Haverkort <dchaverkort@gmail.com>
2023-11-30 16:48:58 +0000

    website & variable_path

--- variable_paths.py
+++ variable_paths.py
@@ -1,3 +1,8 @@
+'''
+----------------------------
+Path variables
+-----------------------------
+'''
 
-
-ckpt =
+local_composer_ckpt = '/Users/d.haverkort/code/DHaverkort/00 - MIDI-BERT-CP/Results/Finetune/melody_default/composer_best.ckpt'
+cp_dict = '/music_project/bert/CP.pkl'
